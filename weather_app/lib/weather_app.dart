f80d01cecde0404b6ccf443f3f20de817c9e6d65
deependrakashya <dipendrakashyap493@gmail.com>
2024-08-07 18:42:59 +0530

    final app added

--- weather_app/lib/weather_app.dart
+++ weather_app/lib/weather_app.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -23,7 +24,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
   Future<Map<String, dynamic>> getCurrentWeather() async {
     try {
       final res = await http.get(Uri.parse(
-         'https://api.openweathermap.org/data/2.5/forecast?q=Noida,Ind&appid=d88c156b70ec0c5b561740d2ff3b9d52'));
+          'https://api.openweathermap.org/data/2.5/forecast?q=Noida,Ind&appid=d88c156b70ec0c5b561740d2ff3b9d52'));
       final data = jsonDecode(res.body);
       if (data['cod'] != '200') {
         throw 'An unexpected error occurred';
@@ -47,7 +48,12 @@ class _WeatherScreenState extends State<WeatherScreen> {
         actions: [
           Container(
               margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)))
+              child:
+                  IconButton(onPressed: () {
+                    setState(() {
+                      getCurrentWeather();
+                    });
+                  }, icon: const Icon(Icons.refresh)))
         ],
       ),
       body: FutureBuilder<Map<String, dynamic>>(
@@ -65,6 +71,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
           final currentHumidity = data['list'][0]['main']['humidity'];
           final currentSky = data['list'][0]['weather'][0]['main'];
           final windSpeed = data['list'][0]['wind']['speed'];
+
           final allList = data['list'];
 
           currentTemp -= 273.15;
@@ -82,7 +89,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                     child: Card(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)),
-                      elevation: 15,
+                      elevation: 10,
                       child: ClipRRect(
                         // just for adding border radius
                         borderRadius: BorderRadius.circular(10),
@@ -95,7 +102,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                 Text(
                                   '${currentTemp.toStringAsFixed(2)} °C',
                                   style: const TextStyle(
-                                      fontSize: 30, fontWeight: FontWeight.bold),
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
                                 ),
                                 const SizedBox(
                                   height: 10,
@@ -123,7 +131,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                   const SizedBox(
                     height: 30,
                   ),
-            
+
                   const Text(
                     'Hourly Forecast',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
@@ -131,21 +139,47 @@ class _WeatherScreenState extends State<WeatherScreen> {
                   const SizedBox(
                     height: 10,
                   ),
-            
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        for (int i = 1; i < 10; i++)
-                          HourlyForecast(
-                            details: allList[i]['weather'][0]['description'].toString(),
-                            time: allList[i]['dt_txt'].toString(),
-                            temp: (allList[i]['main']['temp'] - 273.15).toStringAsFixed(2),
-                          )
-                      ],
+
+                  // SingleChildScrollView(
+                  //   scrollDirection: Axis.horizontal,
+                  //   child: Row(
+                  //     children: [
+                  //       for (int i = 1; i < 10; i++)
+
+                  //         HourlyForecast(
+                  //           details: allList[i]['weather'][0]['description'].toString(),
+                  //           time:allList[i]['dt_txt'].toString(),
+                  //           temp: (allList[i]['main']['temp'] - 273.15).toStringAsFixed(2),
+                  //         )
+                  //     ],
+                  //   ),
+                  // ),
+
+                  SizedBox(
+                    height: 190,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 9,
+                      itemBuilder: (context, index) {
+                        final hourlyForcast =
+                            DateTime.parse(allList[index + 1]['dt_txt']);
+                        final hCWeather =
+                            allList[index + 1]['weather'][0]['main'];
+
+                        return HourlyForecast(
+                          details: allList[index + 1]['weather'][0]
+                                  ['description']
+                              .toString(),
+                          time: DateFormat.Hm().format(hourlyForcast),
+                          temp: (allList[index + 1]['main']['temp'] - 273.15)
+                              .toStringAsFixed(2),
+                          icon: hCWeather == 'Rain' || hCWeather == 'Clouds'
+                              ? Icons.cloud
+                              : Icons.sunny,
+                        );
+                      },
                     ),
                   ),
-            
                   const SizedBox(
                     height: 30,
                   ),
@@ -186,20 +220,23 @@ class HourlyForecast extends StatelessWidget {
   final String time;
   final String details;
   final String temp;
+  final IconData icon;
 
   const HourlyForecast(
       {super.key,
       required this.time,
       required this.details,
-      required this.temp});
+      required this.temp,
+      required this.icon});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5),
-    
+
       // width: 100,
       child: Card(
+        elevation: 5,
         child: Container(
           padding: const EdgeInsets.all(20),
           child: Column(
@@ -208,18 +245,24 @@ class HourlyForecast extends StatelessWidget {
                 time,
                 style:
                     const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
               const SizedBox(
                 height: 10,
               ),
-              const Icon(
-                Icons.cloud,
+              Icon(
+                icon,
                 size: 30,
               ),
               const SizedBox(
                 height: 10,
               ),
               Text('$temp°C'),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(details),
             ],
           ),
         ),
@@ -241,20 +284,23 @@ class AdditionalInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Icon(iconName),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(name),
-          Text(value)
-        ],
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Icon(iconName),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(name),
+            Text(value)
+          ],
+        ),
       ),
     );
   }
